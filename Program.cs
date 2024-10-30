94332751b4a789d9548d68405870ed837af5b655
Joseph Gitonga <josephkimanigitonga370@gmail.com>
2024-10-30 08:59:50 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+
+
+
+    static void Main()
+    {
+        Console.WriteLine("Input a phone number to check whether its is a correct format");
+        string input = Console.ReadLine();
+
+        Match match = Regex.Match(input, @"\d{3}-\d{3}-\d{3}");
+
+
+        if (match != null)
+        {
+
+            Console.WriteLine(match);
+
+
+
+        }
+        Console.WriteLine("Invalid phone number formart");
+
+
+
+    }
+}
